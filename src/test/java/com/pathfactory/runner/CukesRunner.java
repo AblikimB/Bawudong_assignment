e8b288e9f53884e12e15480a4c65a0ded2b5ffb3
Bawudong Abulikemu <a.bawudun@gmail.com>
2023-08-17 07:39:38 -0400

    new branch is created

---
+++ src/test/java/com/pathfactory/runner/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.pathfactory.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/features",
+        glue = "com/pathfactory/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+public class CukesRunner {
+
+}
